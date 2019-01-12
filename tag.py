444f2e5bb5ce39a63b246c9651488401541d1a67
Primoz Belej <pbelej90@gmail.com>
2019-01-12 08:36:31 +0100

    Implementiral oznacevanje vert datotek.

--- tag.py
+++ tag.py
@@ -1,10 +1,15 @@
 import numpy as np
 import neuralmodel
 import poslib
+import argparse
+import os
 
 
-def read_vert_file(filename):
+def read_vert_file(filename, fields=(0, 2)):
     contents = open(filename).readlines()
+
+    result = [[] for f in fields]
+    sentence_values = [[] for f in fields]
 
     sents = []
     sent = []
@@ -12,21 +17,24 @@ def read_vert_file(filename):
     tags = []
     sent_tags = []
 
-    for row in contents:
-        row = row.strip().split('\t')
-        if len(row) == 3:
-            sent.append(row[0])
-            sent_tags.append(row[2])
-        else:
-            sents.append(sent)
-            sent = []
-            tags.append(sent_tags)
-            sent_tags = []
+    for row_index, row in enumerate(contents):
+        if len(row.strip()) == 0:
+            for i, sv in enumerate(sentence_values):
+                result[i].append(sv)
+            sentence_values = [[] for f in fields]
+            continue
 
-    if sent:
-        sents.append(sent)
-        tags.append(sent_tags)
-    return sents, tags
+        split_row = row.strip().split('\t')
+        if len(split_row) < max(fields)+1:
+            raise IndexError("Unexpected number of columns in row {} of file "
+                             "{}.".format(row_index, filename))
+        for field_index, field in enumerate(fields):
+            sentence_values[field_index].append(split_row[field].strip())
+
+    if sentence_values[0]:
+        for i, sv in enumerate(sentence_values):
+            result[i].append(sv)
+    return result
 
 
 def filter_sents(sents, tags, maxlen_word, maxlen_sent):
@@ -110,17 +118,17 @@ def mostprobablepos(embeddings, pos_index, prediction):
     return pos_index[np.argmax(embeddings.dot(prediction))]
 
 
-def prediction2tags(prediction, embedding_dict):
+def prediction2tags(prediction, embedding_dict, local_pred2tag=True):
     tags = list()
     pos_index, embeddings = posembeddings()
     for i in range(prediction.shape[0]):
-        binary = binarize(prediction[i, :], list(poslib.PROPERTY_INDEX.values()))
-        str_emb = ''.join(list(binary.astype('str')))
-        if str_emb in embedding_dict:
-            tags.append(embedding_dict.get(str_emb))
-        else:
-            tags.append(
-                mostprobablepos(embeddings, pos_index, prediction[i, :]))
+        sent_tags = mostprobablepos(embeddings, pos_index, prediction[i, :])
+        if local_pred2tag:
+            binary = binarize(prediction[i, :], list(poslib.PROPERTY_INDEX.values()))
+            str_emb = ''.join(list(binary.astype('str')))
+            if str_emb in embedding_dict:
+                sent_tags = embedding_dict.get(str_emb)
+        tags.append(sent_tags)
     return tags
 
 
@@ -135,31 +143,92 @@ def posembeddings():
     return pos_index, pos_vector
 
 
-def main():
-    # Omejitve dolzin besed in povedi
+def write_vert(path, sentences, tags):
+    assert len(sentences) == len(tags)
+    with open(path, 'w') as outfile:
+        for sentence_index, sentence in enumerate(sentences):
+            sentence_tags = tags[sentence_index]
+            assert len(sentence_tags) == len(sentence)
+            for word_index, word in enumerate(sentence):
+                outfile.write(word)
+                outfile.write('\t')
+                outfile.write(sentence_tags[word_index])
+                outfile.write('\n')
+            outfile.write('\n')
+
+
+
+def predict_tags(sentences, model):
     maxlen_word = 20
     maxlen_sent = 70
+    character_dict = load_character_dict('./characterlist')
+    character_dim = len(character_dict)
+    tag_dict = load_tag_dict('./pos_embeddings')
+    tag_dim = len(list(tag_dict.values())[0])
+    x = vectorize_sentences(sentences, maxlen_sent, maxlen_word, character_dict)
+    y_predicted = model.predict(x)
+    embedding_dict = {}
+    predictions = []
+    with open('./pos_embeddings') as infile:
+        for line in infile:
+            pos, embedding = line.strip().split(', ')
+            embedding_dict[embedding] = pos
+    for i, prediction in enumerate(y_predicted):
+        predictions.append(
+            prediction2tags(prediction[:len(sentences[i]), :], embedding_dict))
+    return predictions
+
+
+def get_sentences_txt(file_path):
+    pass
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", help="Path to input file.")
+    parser.add_argument("output", help="Path to output file.")
+    parser.add_argument("-f", "--force", help="Overwrite output file.",
+                        action="store_true")
+    args = parser.parse_args()
+
+    if not os.path.exists(args.input):
+        print("File {} does not exist.".format(args.input))
+        exit()
+
+    if os.path.exists(args.output) and not args.force:
+        print("Output file {} already exist. "
+              "Output file can be overwriten by passing "
+              "-f argument.".format(args.output))
+        exit()
+
+    if args.input.endswith(".vert"):
+        sentences = read_vert_file(args.input, fields=(0,))[0]
+    elif args.input.endswith(".txt"):
+        sentences = get_sentences_txt(args.input)
+    else:
+        print("Invalid input file extension. "
+              "Valid input types are vert and txt.")
+        exit()
+
+    model = neuralmodel.load_model(
+        './model_5fold.json',
+        #'./model_10_1.h5'
+        './model_celotni_podatki.h5'
+    )
+    #sents, tags = filter_sents(sents, tags, maxlen_word, maxlen_sent)
+
+    #predictions = predict_tags([['Moj', 'pes', 'je', 'zelo', 'lep', '.']], model)
+    sentences = sentences[:3]
+    predictions = predict_tags(sentences[:3], model)
+    write_vert(args.output, sentences, predictions)
+
+    # Omejitve dolzin besed in povedi
 
     # Podatke preberemo iz datoteke vert
     #sents, tags = read_vert_file('./podatki/fold1_test.vert')
 
     # Odstranimo predolge povedi
-    #sents, tags = filter_sents(sents, tags, maxlen_word, maxlen_sent)
-
-    # Povedi in oznake problikujemo v vektorsko obliko
-    character_dict = load_character_dict('./characterlist')
-    character_dim = len(character_dict)
-    tag_dict = load_tag_dict('./pos_embeddings')
-    tag_dim = len(list(tag_dict.values())[0])
-
-    sentences = [['Moj', 'pes', 'je', 'zelo', 'lep', '.']]
-    x = vectorize_sentences(sentences, maxlen_sent, maxlen_word, character_dict)
-
-    model = neuralmodel.load_model(
-        './model_5fold.json',
-        './model_10fold_fold1.h5')
     #model = neuralmodel.build_model(x[0], character_dim, tag_dim)
-    y_predicted = model.predict(x[:1])
 
     #x, y = load_data(sents, tags, maxlen_word, maxlen_sent, character_dict,
     #                 tag_dict)
@@ -178,26 +247,6 @@ def main():
 
 
     # Napovemo oznake testne povedi
-
-    embedding_dict = {}
-    with open('./pos_embeddings') as infile:
-        for line in infile:
-            pos, embedding = line.strip().split(', ')
-            embedding_dict[embedding] = pos
-    return y_predicted, embedding_dict
-    #for i, prediction in enumerate(y_predicted):
-    #    prediction = prediction2tags(prediction[:len(sentences[i]), :], embedding_dict)
-    #    print(sentences[0])
-    #    print(prediction)
-
-
-    #a = y_predicted[0, :len(sents[0]), :]
-    #print(a.shape)
-    #prediction = prediction2tags(a,
-    #                             embedding_dict)
-
-    #print(tags[0])
-    #print(prediction)
 
 
 if __name__ == '__main__':
